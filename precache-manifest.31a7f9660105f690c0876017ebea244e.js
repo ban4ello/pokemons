f3e2afbd9473bf71d4b13d473d2edf45e04787b5
Ivan Miroshnichenko <ivanmiroshnichenko@MacBook-Air-Ivan.local>
2018-11-16 15:52:42 +0200

    Updates

---
+++ precache-manifest.31a7f9660105f690c0876017ebea244e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pokemons/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "60e941627339790b29e5",
+    "url": "/pokemons/static/css/main.575ff6f6.chunk.css"
+  },
+  {
+    "revision": "b24f7ff985ff171507a6",
+    "url": "/pokemons/static/js/1.b24f7ff9.chunk.js"
+  },
+  {
+    "revision": "962ef7561fdfccf30749",
+    "url": "/pokemons/static/js/runtime~main.962ef756.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pokemons/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "60e941627339790b29e5",
+    "url": "/pokemons/static/js/main.60e94162.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pokemons/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pokemons/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pokemons/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "ad677079af81defa35ba236b99b00ca2",
+    "url": "/pokemons/static/media/icons.ad677079.woff"
+  },
+  {
+    "revision": "9ad537f0428831031a694847aff91998",
+    "url": "/pokemons/index.html"
+  }
+];
